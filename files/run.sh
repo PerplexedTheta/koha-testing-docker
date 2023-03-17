6821e97cc3efc3e78a231a04c2c7a4f0e8b2c0ae
Tomas Cohen Arazi <tomascohen@theke.io>
2023-03-17 10:30:48 -0300

    Issue #365: Empty != 1000 :-D
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- files/run.sh
+++ files/run.sh
@@ -117,8 +117,8 @@ ln -s /kohadevbox/Cypress "/var/lib/koha/${KOHA_INSTANCE}/.cache/" \
   && echo "    [*] Cypress dir linked to /var/lib/koha/${KOHA_INSTANCE}/.cache/" \
   || echo "    [x] Error linking Cypress dir to /var/lib/koha/${KOHA_INSTANCE}/.cache/"
 
-# Fix UID
-if [ "${LOCAL_USER_ID}" != "1000" ]; then
+# Fix UID if not empty, and differs from 1000 (Docker's default for the next UID)
+if [[ ! -z "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "1000" ]]; then
     usermod -o -u ${LOCAL_USER_ID} "${KOHA_INSTANCE}-koha"
 
     chown -R "${KOHA_INSTANCE}-koha:${KOHA_INSTANCE}-koha" "/kohadevbox/Cypress" \
