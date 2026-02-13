94aaac834892cd6340747856656e5fdb4f0412ae
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-02-13 08:06:04 +0000

    Revert "Follow 'tests-only' form for `--run-perl-tests-only`"
    This reverts commit 7d93c9d3d6f1cca9492e06be89ec9529b604cc2b

--- files/run.sh
+++ files/run.sh
@@ -418,7 +418,7 @@ if [ "$RUN_TESTS_AND_EXIT" = "yes" ]; then
 
     elif [ "$TEST_SUITE" = "all-perl-tests" ]; then # all perl tests only
 
-        perl ${BUILD_DIR}/misc4dev/run_tests.pl --koha-dir=${BUILD_DIR}/koha --run-perl-tests-only
+        perl ${BUILD_DIR}/misc4dev/run_tests.pl --koha-dir=${BUILD_DIR}/koha --run-all-perl-tests
 
     elif [ "$TEST_SUITE" = "db-compare-only" ]; then # update the DB, dbic, DB structure
 
